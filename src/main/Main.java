5a315707cc2a22493ce4d5cc3c2379b774867f62
MingoUwU <trungnamnguyen456@gmail.com>
2026-01-28 16:51:25 +0700

    a

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@
 package main;
 
 import controller.MainController;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Main entry point for GroupTrip application
@@ -12,6 +14,13 @@ import controller.MainController;
  */
 public class Main {
     public static void main(String[] args) {
+        // Fix console encoding to prevent character spacing issues
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Warning: UTF-8 encoding not supported");
+        }
+        
         MainController controller = new MainController();
         controller.run();
     }
